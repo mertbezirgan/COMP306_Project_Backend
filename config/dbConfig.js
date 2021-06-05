a01e2fee9093878901a8aebf5150aafb7e50ace1
Ceyhun Aslan <ceyhunaslan18@gmail.com>
2021-06-05 23:35:57 +0300

    formatting

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "",
-database:"imdb",
-port:4406 
-})
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "imdb",
+	port: 4406,
+});
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const db = require('./config/dbConfig');
-const cors = require('cors');
+const express = require("express");
+const db = require("./config/dbConfig");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3002;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    db.query("SELECT * FROM movies WHERE year > 2005", (err, result) =>{
-        if(err){
-            console.log(err)
-        }else{
-            res.send(result)
-        }
-    })
-})
-
+app.get("/", (req, res) => {
+	db.query("SELECT * FROM movies WHERE year > 2005", (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
+	});
+});
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
+	console.log(`Example app listening at http://localhost:${PORT}`);
 });
