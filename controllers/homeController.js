6b1c2ac7635eebb4d39296c2ad6190d4b43d796e
Ceyhun Aslan <ceyhunaslan18@gmail.com>
2021-06-06 01:36:29 +0300

    small fixes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,15 +2,15 @@ const { rs, re, qu } = require("../utils/utils");
 
 const searchMovies = async (req, res) => {
 	let { query, startIndex, length } = req.body;
-    if (!startIndex || !length) return re(res, { message: 'Please give startIndex and length' }, 400);
+    if (startIndex === undefined || length === undefined) return re(res, { message: 'Please give startIndex and length' }, 400);
 
 	let [err, data] = await qu(
 		`select mo.*, mg.genre from movies as mo, movies_genres as mg where ${
 			query ? `name like "%${query}%" and` : ""
 		} mo.id = mg.movie_id order by id limit ${length} offset ${startIndex}`
 	);
-	if (err) re(res, err);
+	if (err) return re(res, err);
 
-	rs(res, data);
+	return rs(res, data);
 };
 module.exports.searchMovies = searchMovies;
